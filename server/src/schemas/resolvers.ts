7d67970953212a8b8bc99a9f65b8a61dac4178e2
Jacob Garland <352cjg@gmail.com>
2025-03-05 20:30:29 -0500

    fixed backend resolvers

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,17 +1,28 @@
 import User from '../models/User.js';
 import { signToken } from '../services/auth.js';
+import { AuthenticationError } from 'apollo-server-express';
+
+interface Context {
+  user?: { _id: string };
+}
 
 const resolvers = {
     Query: {
-      getSingleUser: async (_: any, variables: { id?: string; username?: string }, req: any) => {
+      me: async (_: any, __: any, context: Context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id });
+        }
+        throw new Error('You need to be logged in!');
+      },
+      getSingleUser: async (_: any, args: { id?: string; username?: string }, context: Context) => {
         const foundUser = await User.findOne({
           $or: [
-            { _id: req.user ? req.user._id : variables.id },
-            { username: req.user ? req.user.username : variables.username },
+            { _id: context.user ? context.user._id : args.id },
+            { username: args.username },
           ],
         });
         if (!foundUser) {
-          throw new Error('Cannot find a user with this id!');
+          throw new AuthenticationError('Cannot find a user with this id!');
         }
         return foundUser;
       },
@@ -24,6 +35,34 @@ const resolvers = {
         }
         const token = signToken(user.username, user.password, user._id);
         return { token, user };
+      },
+      login: async (_: any, args: { username: string; password: string }) => {
+        const user = await User.findOne({ username: args.username });
+        if (!user || !(await user.isCorrectPassword(args.password))) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        const token = signToken(user.username, user.password, user._id);
+        return { token, user };
+      },
+      saveBook: async (_: any, args: { bookId: string; title: string }, context: Context) => {
+        if (!context.user) {
+          throw new AuthenticationError('Not authenticated');
+        }
+        return await User.findByIdAndUpdate(
+          context.user._id,
+          { $addToSet: { savedBooks: args } },
+          { new: true, runValidators: true }
+        );
+      },
+      removeBook: async (_: any, args: { bookId: string }, context: Context) => {
+        if (!context.user) {
+          throw new AuthenticationError('Not authenticated');
+        }
+        return await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
       },
     },
 };
